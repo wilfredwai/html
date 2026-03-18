a24f3f05cdcb5efe087c2853cde461cdf3770f45
wilfredwai <wilfredwilfred@gmail.com>
2026-03-18 15:16:46 +0800

    Change directory location

--- public/src/main.py
+++
@@ -1,2 +0,0 @@
-./main.sh
-print("hello world")
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextType, TextNode
+
+def main():
+    testobject = TextNode("This is a test in plain text", TextType.BOLD)
+    print(testobject)
+main()
