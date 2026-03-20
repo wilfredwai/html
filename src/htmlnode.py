cac4b3f49fb53affa94758c89b394eff89f97367
wilfredwai <wilfredwilfred@gmail.com>
2026-03-20 12:06:07 +0800

    add ParentNode but did not finish yet

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,13 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Error: Missing tag value!")
+        if self.children is None:
+            raise ValueError("Error: Missing children value")
+        return 
