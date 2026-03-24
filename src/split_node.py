8784fe5a65f7a617eba5346b8620c918dd1af8d7
wilfredwai <wilfredwilfred@gmail.com>
2026-03-24 17:29:02 +0800

    update split_node

--- src/split_node.py
+++ src/split_node.py
@@ -2,11 +2,22 @@ from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_list = []
-    delimiters = ["'", "**", "_"]
-    for nodes in old_nodes:
-        if not nodes.text_type is TextType.TEXT:
-            new_list.append(nodes)
-        for dels in delimiters:
-            if dels not in nodes.text:
-                raise Exception("invalid Markdown syntax, no delimiter found in the node")
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_list.append(node)
+            return new_list
+        split_node = node.text.split(delimiter)
+        if len(split_node) == 1:
+            new_list.append(TextNode(split_node), TextNode.TEXT)
+            return new_list
+        elif len(split_node) %2 != 0:
+            raise Exception("invalid Markdown syntax")
+        else:
+            for i in range(0, len(split_node), 2):
+                new_list.append(TextNode(split_node[0], TextType.TEXT))
+                new_list.append(TextNode(split_node[1], text_type))
+                new_list.append(TextNode(split_node[2], TextType.TEXT))
         
+            
+    return new_list
