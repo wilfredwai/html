83c7123b820eea5bd7169c325cc7694cb3ece4e0
wilfredwai <wilfredwilfred@gmail.com>
2026-04-23 16:59:23 +0800

    keepgoing

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,10 +1,31 @@
 from markdown_to_blocks import markdown_to_blocks
 from blocktype import block_to_block_type, BlockType
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from text_to_textnodes import text_to_textnodes
+from textnode import text_node_to_html_node
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    children = []
     for block in blocks:
-        block_type = block_to_block_type(block)
-        if block_type == BlockType.QUOTE:
-            
+        html_node = block_to_html_node(block)
+        children.appended(html_node)
+    return ParentNode("div", children)
+
+def text_to_children(text):
+        text_nodes = text_to_textnodes(text)
+        children = []
+        for text_node in text_nodes:
+            html_node = text_node_to_html_node(text_node)
+            children.append(html_node)
+        return children
+
+def paragraph_to_html_node(block):
+    clean_text = block.replace("\n", " ")
+    children = text_to_children(clean_text)
+    return ParentNode(tag="p", children = children)
+     
+def heading_to_html_node(block):
+    count = block.count("#")
+    if count == 0:
+        return block
