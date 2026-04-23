8296f89db3975fd89833ba63f9c8130a48bdc774
wilfredwai <wilfredwilfred@gmail.com>
2026-04-23 08:53:19 +0800

    Working on markdown to html node

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,8 +1,10 @@
 from markdown_to_blocks import markdown_to_blocks
-from blocktype import block_to_block_type
+from blocktype import block_to_block_type, BlockType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
         block_type = block_to_block_type(block)
+        if block_type == BlockType.QUOTE:
+            
