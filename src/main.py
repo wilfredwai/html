d7d4a092df39eda3c1a654c1429746bea06c46cb
wilfredwai <wilfredwilfred@gmail.com>
2026-04-24 16:59:19 +0800

    working on main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import os
 from textnode import TextType, TextNode
 
 def main():
-    testobject = TextNode("This is a test in plain text", TextType.BOLD)
-    print(testobject)
+    if os.path.exists("./public") and os.path.exists("./src/static"):
+        
+    else:
+        print("Error, path not exists!")
 main()
